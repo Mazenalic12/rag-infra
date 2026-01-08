13262a022544e1120a2e64a16fbb3ab370885ba9
536351-Mazenalic-Feb22-Python101 <160591633+Mazenalic12@users.noreply.github.com>
2026-01-08 15:37:32 +0100

    Update main.tf

--- infrastructure/terraform/eks/main.tf
+++ infrastructure/terraform/eks/main.tf
@@ -20,8 +20,8 @@ provider "aws" {
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "JOUW_TF_STATE_BUCKET"
-    key    = "rag/vpc/terraform.tfstate"
+    bucket = var.tf_state_bucket
+    key    = var.vpc_state_key
     region = var.region
   }
 }
